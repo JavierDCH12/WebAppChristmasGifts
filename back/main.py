9d5c24a4eb4f6de98ea6f704d89b07b6e1d32a8f
Javier Delgado <javier.delgado@viewnext.com>
2024-12-20 12:22:28 +0100

    added CORSMiddleware

--- back/main.py
+++ back/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routes.auth import auth_router
 from routes.book_recommendation import book_recommendation_router
 from routes.game_recommendations import game_recommendation_router
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(game_recommendation_router, prefix="/api", )
 app.include_router(book_recommendation_router, prefix="/api")
