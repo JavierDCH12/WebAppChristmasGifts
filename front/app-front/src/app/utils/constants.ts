35d9855a67d883c1e97083de31ab5a2452ea09be
JavierDCH12 <javierdelgadoch95@gmail.com>
2025-01-03 16:34:30 +0100

    added CONSTANT ROUTES

---
+++ front/app-front/src/app/utils/constants.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: '/home',
+  BOOKS: '/books',
+  GAMES: '/games',
+  LOGIN: '/login',
+  RECOMMENDATIONS: {
+    BOOKS: '/recommendations/books',
+    GAMES: '/recommendations/games'
+  }
+};
